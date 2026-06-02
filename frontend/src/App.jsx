0404f6576105d19c13348712cbceeb2ca4b38bff
Jana Saiyam Santosh <saiyamjana713@gmail.com>
2026-06-02 20:02:40 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+    return (
+        <div className="StartPage">
+            <h1 className="text-4xl font-bold mb-4">Welcome to the Start Page</h1>
+            <p className="text-lg">This is a simple React application.</p>
+        </div>
+    )
+}
+
+export default App
