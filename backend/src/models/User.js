f439c1ac80136218e3d802a88bdc466d0ce4f76b
Jana Saiyam Santosh <saiyamjana713@gmail.com>
2026-06-04 11:51:07 +0530

    database update

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -52,4 +52,33 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign({
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+      role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,   // e.g. "1d"
+    }
+  );
+};
+ 
+// ─── Generate Refresh Token ───────────────────────────────────────────────────
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,  // e.g. "7d"
+    }
+  );
+};
+
+
 export const User = mongoose.model("User", userSchema);
