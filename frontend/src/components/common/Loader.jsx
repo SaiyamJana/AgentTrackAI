15d480cda7b53ba849cd80fb309356897b55c421
Anamika <anamikasingh721454@gmail.com>
2026-06-05 12:17:29 +0530

    Resolved merge conflicts

--- frontend/src/components/common/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex justify-center items-center p-10">
-      <div className="h-12 w-12 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-};
-
-export default Loader;
